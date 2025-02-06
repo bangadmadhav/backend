2fae43046d218478f5af19f369f74c1e89629cf9
bangadmadhav <bangadmadhav@gmail.com>
2025-02-07 02:07:12 +0530

    Added some util files to handle apiError and apiresponse also, a asyncHandler util

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({path:'.env'})
 
-connectDB();
+const port = process.env.PORT || 3000;
+
+connectDB()
+.then(
+    app.listen(port, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+)
+.catch((error)=> {
+    console.error("Database Connection Failed: ", error);
+});
